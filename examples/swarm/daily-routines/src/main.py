32de87eba9bbed86fca9fdc555d68f7c27c0a8f1
Emanuel <e.w.degraaff@gmail.com>
2025-01-20 23:57:01 +0100

    docs(examples): add daily-routines example

---
+++ examples/swarm/daily-routines/src/main.py
@@ -0,0 +1,112 @@
+from swarm import Swarm
+from datetime import datetime
+import pytz
+
+from murmur.clients.swarm import SwarmAgent
+from murmur.agents import daily_routine, science_paper_explainer, friendly_assistant
+from utils import run_demo_loop
+
+############################################################
+# Instantiate Swarm 🐝
+############################################################
+
+client = Swarm()
+
+############################################################
+# Tools
+############################################################
+
+from murmur.tools import get_open_meteo_weather, get_bitcoin_exchange_rate, get_arxiv_paper    
+
+############################################################
+# Context Variables
+############################################################
+
+system_prompt = f"""
+    - Only greet the user once.
+    - Help users with their routines, if any. 
+    - Routines are time specific. 
+    - Important: Do not say goodbye.
+    - Current time is {datetime.now(pytz.timezone('America/Los_Angeles'))}
+"""
+
+user_context = """
+    Here is what you know about the user:
+    - NAME: Joe
+    - CURRENCY: USD
+    - LOCATION: 34.0549° N, 118.2426° W
+    - TEMPERATURE_UNIT: fahrenheit
+    - TIMEZONE: America/Los_Angeles
+    - SCIENCE_TOPIC: genai
+"""
+
+orchestration_context = """
+    1. Morning Routine
+        1.1. Get the weather
+        1.2. Get the Bitcoin exchange rate
+        1.3. Get the science paper
+    2. Afternoon Routine
+        2.1. Get the weather
+        2.3. Get the science paper
+    3. Evening Routine
+        3.2. Get the Bitcoin exchange rate
+        3.3. Get the science paper
+"""
+
+context_variables = {
+    'system_prompt': system_prompt,
+    'user_context': user_context,
+    'routines': orchestration_context
+}
+
+instructions = list(context_variables.values())
+
+############################################################
+# Agents
+############################################################
+
+friendly_assistant_agent = SwarmAgent(
+    friendly_assistant,
+    instructions=instructions
+)
+
+daily_routine_agent = SwarmAgent(
+    daily_routine,
+    instructions=instructions,
+    tools=[get_open_meteo_weather, get_bitcoin_exchange_rate]
+)
+
+science_paper_explainer_agent = SwarmAgent(
+    science_paper_explainer,
+    instructions=instructions,
+    tools=[get_arxiv_paper]
+)
+
+############################################################
+# Hand-off
+############################################################
+
+def transfer_to_daily_routine_agent():
+    """Transfer daily routine requesting users."""
+    return daily_routine_agent
+
+def transfer_to_science_paper_explainer_agent():
+    """Transfer science paper explainer requests."""
+    return science_paper_explainer_agent
+
+def transfer_to_friendly_assistant_agent():
+    """Transfer science paper explainer requests."""
+    return friendly_assistant_agent
+
+friendly_assistant_agent.functions = [transfer_to_daily_routine_agent, science_paper_explainer_agent]
+
+daily_routine_agent.functions.append(transfer_to_science_paper_explainer_agent)
+
+science_paper_explainer_agent.functions.append(transfer_to_friendly_assistant_agent)
+
+############################################################
+# Loop
+############################################################
+
+if __name__ == "__main__":
+    run_demo_loop(client, friendly_assistant_agent, context_variables=context_variables, debug=True)
